049fc14acb50ec6660fdb25c3baeca1b49e38cde
Naheel-Azawy <naheelazawy@gmail.com>
2020-03-25 04:02:52 +0300

    quick update

--- main.js
+++ main.js
@@ -27,14 +27,20 @@ var saveData = JSON.parse(localStorage.saveData || null) || {};
 // Store your data.
 function saveStuff(obj) {
     saveData.obj = obj;
-    // saveData.foo = foo;
     saveData.time = new Date().getTime();
     localStorage.saveData = JSON.stringify(saveData);
 }
 
 // Do something with your data.
 function loadStuff() {
-    return saveData.obj || {src: defaultCode};
+    var url = new URL(window.location.href);
+    var urlSrc = decodeURI(url.searchParams.get("src"));
+    if (urlSrc && urlSrc != "null")
+        return {src: urlSrc};
+    else if (saveData.obj && saveData.obj.src && saveData.obj.src != "null")
+        return saveData.obj;
+    else
+        return {src: defaultCode};
 }
 
 var editor = CodeMirror.fromTextArea(document.querySelector('textarea'), {
@@ -46,6 +52,49 @@ function run() {
     con.innerHTML = "";
     var code = editor.getValue();
     eval(code);
+}
+
+// https://stackoverflow.com/questions/400212/how-do-i-copy-to-the-clipboard-in-javascript
+
+function fallbackCopyTextToClipboard(text) {
+    var textArea = document.createElement("textarea");
+    textArea.value = text;
+
+    // Avoid scrolling to bottom
+    textArea.style.top = "0";
+    textArea.style.left = "0";
+    textArea.style.position = "fixed";
+
+    document.body.appendChild(textArea);
+    textArea.focus();
+    textArea.select();
+
+    try {
+        var successful = document.execCommand('copy');
+        var msg = successful ? 'successful' : 'unsuccessful';
+        console.log('Fallback: Copying text command was ' + msg);
+    } catch (err) {
+        console.error('Fallback: Oops, unable to copy', err);
+    }
+
+    document.body.removeChild(textArea);
+}
+function copyTextToClipboard(text) {
+    if (!navigator.clipboard) {
+        fallbackCopyTextToClipboard(text);
+        return;
+    }
+    navigator.clipboard.writeText(text).then(function() {
+        console.log('Async: Copying to clipboard was successful!');
+    }, function(err) {
+        console.error('Async: Could not copy text: ', err);
+    });
+}
+
+function copy() {
+    var url = "https://naheel-azawy.github.io/boat-code?src=";
+    url += encodeURI(editor.getValue());
+    copyTextToClipboard(url);
 }
 
 editor.setValue(loadStuff().src);
